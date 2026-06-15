37063f1b7e1a15c52e1b50a443f5e823b196a626
cain0508 <k9774438@gmail.com>
2026-06-16 00:15:32 +0530

    bst ques 300

---
+++ BST/question_300.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>tails;
+        for(int num:nums){
+            auto numb = lower_bound(tails.begin(),tails.end(),num);
+            if(numb==tails.end()) tails.push_back(num);
+            else *numb=num;
+        }
+        return tails.size();
+    }
+};
