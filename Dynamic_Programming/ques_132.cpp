da35c29b15e68c5b7a9c319bb0762eee9ed8da4e
cain0508 <k9774438@gmail.com>
2026-06-19 01:16:30 +0530

    solved question 132 using dp

---
+++ Dynamic_Programming/ques_132.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int maxP=nums[0];
+        int minP=nums[0];
+        int ans=nums[0];
+        for(int i=1;i<nums.size();i++){
+            int temp = maxP;
+            maxP= max({nums[i],maxP*nums[i],minP*nums[i]});
+            minP= min({nums[i],temp*nums[i],minP*nums[i]});
+            ans = max(ans,maxP);
+        }
+        return ans;
+    }
+};
