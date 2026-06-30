5445658f78b593e6cc33da260d21f8969b5e01b5
cain0508 <k9774438@gmail.com>
2026-06-30 20:41:53 +0530

    solved

---
+++ Sliding_Window/ques_424.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int freq[26]={0};
+        int left=0,maxFreq=0,result=0;
+        for(int right =0;right<s.size();right++){
+            //ASCII difference 
+            freq[s[right]-'A']++;
+            maxFreq=max(maxFreq,freq[s[right]-'A']);
+
+            while((right-left+1)-maxFreq>k){
+                //valid condition is windowsize-maxfreq<=k
+                freq[s[left]-'A']--;
+                left++;
+            }
+            result=max(result,right-left+1);
+        }
+        return result;
+    }
+};
